ba7149d112b59765743e748bcba7e5b1749a9474
Nikolai <mumunick1@gmail.com>
2023-09-16 19:22:58 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int digit = 32;
+        System.out.println("Your digit is 32");
+        if ((digit % 2) == 0) {
+            System.out.println("Your digit is even");
+        } else {
+            System.out.println("Your digit is odd");
+        }
+    }
+}
